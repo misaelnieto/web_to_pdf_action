e5ac01ef91c1951f0bf4bfd425f516a42ef7ce75
Noe Nieto <noe.nieto@puri.sm>
2020-05-26 14:55:29 -0700

    Lets put puppeteer aside for a moment and use chrome directly

--- action.js
+++ action.js
@@ -4,27 +4,45 @@ const puppeteer = require('puppeteer');
 
 (async () => {
     try {
+        const googlePath = '/opt/google/chrome/chrome';
         const webPageURL = core.getInput('webPageURL');
         const outputFile = core.getInput('outputFile');
-        const pdfDefaults = {
-            'displayHeaderFooter': false,
-            'path': outputFile
-        };
-        const pdfOpts = Object.assign({}, core.getInput('pdfOpts'), pdfDefaults);
-        const pptrOpts = {
-            executablePath: '/opt/google/chrome/chrome',
-            args: ['--no-sandbox', '--headless', '--disable-gpu']
-        };
+        const usePuppeteer = core.getInput('usePuppeteer');
         console.log(`Starting PDF generation for ${webPageURL}`);
+        if (usePuppeteer) {
+            const pdfDefaults = {
+                'displayHeaderFooter': false,
+                'path': outputFile
+            };
+            const pdfOpts = Object.assign({}, core.getInput('pdfOpts'), pdfDefaults);
+            const pptrOpts = {
+                executablePath: googlePath,
+                args: ['--no-sandbox', '--headless', '--disable-gpu']
+            };
 
-        const browser = await puppeteer.launch(pptrOpts);
-        const daPage = await browser.newPage();
-        await daPage.goto(webPageURL, {
-            waitUntil: "networkidle0",
-        })
+            const browser = await puppeteer.launch(pptrOpts);
+            const daPage = await browser.newPage();
+            await daPage.goto(webPageURL, {
+                waitUntil: "networkidle0",
+            })
 
-        await daPage.pdf(pdfOpts)
-        await browser.close()
+            await daPage.pdf(pdfOpts)
+            await browser.close()
+
+        } else {
+            const { exec } = require("child_process");
+            exec(`${googlePath} --no-sandbox --headless --disable-gpu --print-to-pdf="${outputFile}" ${webPageURL}`, (error, stdout, stderr) => {
+                if (error) {
+                    console.log(`error: ${error.message}`);
+                    return;
+                }
+                if (stderr) {
+                    console.log(`stderr: ${stderr}`);
+                    return;
+                }
+                console.log(`stdout: ${stdout}`);
+            });
+        }
         console.log(`DONE. Generated PDf file is at ${outputFile}`)
     }
     catch (error) {
--- dist/index.js
+++ dist/index.js
@@ -21262,27 +21262,45 @@ const puppeteer = __webpack_require__(885);
 
 (async () => {
     try {
+        const googlePath = '/opt/google/chrome/chrome';
         const webPageURL = core.getInput('webPageURL');
         const outputFile = core.getInput('outputFile');
-        const pdfDefaults = {
-            'displayHeaderFooter': false,
-            'path': outputFile
-        };
-        const pdfOpts = Object.assign({}, core.getInput('pdfOpts'), pdfDefaults);
-        const pptrOpts = {
-            // executablePath: '/usr/bin/google-chrome-stable',
-            args: ['--no-sandbox', '--headless', '--disable-gpu']
-        };
+        const usePuppeteer = core.getInput('usePuppeteer');
         console.log(`Starting PDF generation for ${webPageURL}`);
+        if (usePuppeteer) {
+            const pdfDefaults = {
+                'displayHeaderFooter': false,
+                'path': outputFile
+            };
+            const pdfOpts = Object.assign({}, core.getInput('pdfOpts'), pdfDefaults);
+            const pptrOpts = {
+                executablePath: googlePath,
+                args: ['--no-sandbox', '--headless', '--disable-gpu']
+            };
 
-        const browser = await puppeteer.launch(pptrOpts);
-        const daPage = await browser.newPage();
-        await daPage.goto(webPageURL, {
-            waitUntil: "networkidle0",
-        })
+            const browser = await puppeteer.launch(pptrOpts);
+            const daPage = await browser.newPage();
+            await daPage.goto(webPageURL, {
+                waitUntil: "networkidle0",
+            })
 
-        await daPage.pdf(pdfOpts)
-        await browser.close()
+            await daPage.pdf(pdfOpts)
+            await browser.close()
+
+        } else {
+            const { exec } = __webpack_require__(129);
+            exec(`${googlePath} --no-sandbox --headless --disable-gpu --print-to-pdf="${outputFile}" ${webPageURL}`, (error, stdout, stderr) => {
+                if (error) {
+                    console.log(`error: ${error.message}`);
+                    return;
+                }
+                if (stderr) {
+                    console.log(`stderr: ${stderr}`);
+                    return;
+                }
+                console.log(`stdout: ${stdout}`);
+            });
+        }
         console.log(`DONE. Generated PDf file is at ${outputFile}`)
     }
     catch (error) {
